bb917413d4a77a76f4c77a6154242d4489e7d32d
CarlosOlvera <carloscmasmas@gmail.com>
2020-02-04 19:54:31 -0600

    esta es la primera actualizacion

---
+++ pagina1/funcionesjs.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById('lanzar_alerta').onclick = function () {
+        alert('hola mundo!');
+    }
+}
+
+window.onload = function(){
+    this.document.getElementById("hacer_saludo").onclick = function () { 
+        alert("hola" + name);
+    }
+
+
+}
